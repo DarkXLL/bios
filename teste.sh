0983dd672c459b77a16e641ada367c68e6c21d8a
aluno <aluno@Lab-07>
2020-03-05 10:20:50 -0300

    less cometary added

--- teste.sh
+++ teste.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
+#
+#Autores: Mário Jamisson e Fernanda Costa
+#
+#Este script tem como função converter imagens .jpg em .png
+
+echo "Bem-vindo ao JFConversor 1.0"
+echo "Iniciando conversão."
 for img in *.jpg
 do
 	echo $img
 	img_sem_ext=$(ls $img | awk -F '{print $1}')
 	convert $img $img_sem_ext.png
 done
+echo "Finalizando conversão."
 cd ..
